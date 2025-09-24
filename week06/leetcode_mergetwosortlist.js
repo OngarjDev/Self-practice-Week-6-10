b6312d366ac29049b122b48bcb84f5d644234cfc
LT_Ongarj <ongarj2314@gmail.com>
2025-09-24 10:30:43 +0700

    End Week6

--- week06/leetcode_mergetwosortlist.js
+++ week06/leetcode_mergetwosortlist.js
@@ -1,4 +1,14 @@
 /**
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+Input: list1 = [], list2 = [0]
+
+Output: [0]
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
@@ -11,12 +21,28 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let merge = {...list1,...list2}
-    let new_arr = []
-    while(merge){
-        new_arr.push(merge.val)
-        merge = merge.next 
+let new_arr = [];
+    
+    let cur = list1;
+    while (cur) {
+        new_arr.push(cur.val);
+        cur = cur.next;
     }
-    new_arr.sort((a,b) => a - b)
-    /// ข้อมูลเป็น listnode แปลงกลับไม่เป็น พรุ่งนี้ค่อยแก้
+
+    cur = list2;
+    while (cur) {
+        new_arr.push(cur.val);
+        cur = cur.next;
+    }
+
+    new_arr.sort((a, b) => a - b);
+
+    let dummy = new ListNode(0);
+    let tail = dummy;
+    for (let val of new_arr) {
+        tail.next = new ListNode(val);
+        tail = tail.next;
+    }
+
+    return dummy.next;
 }
