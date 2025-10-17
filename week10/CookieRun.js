560e0e3a030b38533d5fb5cef7557d753dfb50c9
LT_Ongarj <ongarj2314@gmail.com>
2025-10-17 20:46:40 +0700

    Learning Storage

--- week10/CookieRun.js
+++ week10/CookieRun.js
@@ -1,7 +1,7 @@
 import { CookieUtil } from './CookieUtil.js'
 console.log(document.cookie)
 document.cookie = `Express=flash_express;expires=${new Date(2025, 9, 15)}`
-document.cookie = `'Meassge'="แอบชอบโอ";expires=${new Date(2098, 9, 15)}`
+document.cookie = `'Meassge'="พัสดุมาส่งแล้วครับ";expires=${new Date(2098, 9, 15)}`
 const obj = new CookieUtil
 console.log(CookieUtil.get("Express"))
 console.log(CookieUtil.set("TestName","TestValue",new Date(2098, 9, 15)))
